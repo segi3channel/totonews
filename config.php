858843046fafe2f8beafbb5c70a454cff8ee9971
segi3channel <44390326+segi3channel@users.noreply.github.com>
2018-12-04 17:50:42 +0700

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 //ikuti format config di bawah "ticket=tiket_kamu&meid=meid_kamu",
 //jangan ada sepasi
 $config = [
-"ticket=sHp2r312otmCpnGns3mCqJhovNiBin-Zr9CjbbSle8u8jXqyina7zo7chWS0iIVlkKO33IqKhM-wuoVptLiGlq2jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ&meid=942471dfcb423d90",
+"ticket=isi_dgn_ticket_kamu&meid=isi_dgn_meid_kamu",
   ];
 
 ?>
